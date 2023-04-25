cb0cc45c3100b01e484ae5ff1f3f1433e585201f
leep1000 <leepalmerformal@gmail.com>
2023-04-25 12:33:13 +0100

    added navigation bar

---
+++ artology/src/components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="nav-bar">
+      <ul>
+        <li>Artist Studio</li>
+        <li>Science Lab</li>
+        <li>Logo</li>
+        <li>Music Massage</li>
+        <li>Code Play</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
