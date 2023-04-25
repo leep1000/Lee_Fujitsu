6671896091418ffa27e602582c2aa3cf352f0b07
Carrie Shen <carrieshen.uk@gmail.com>
2023-04-25 14:30:15 +0100

    two arrays updated

---
+++ artology/RandomItem/Movie.js
@@ -0,0 +1,29 @@
+export let movie = [
+    {
+      id: 1,
+      title: "The Shawshank Redemption",
+      image: " "
+    },
+    {
+      id: 2,
+      title: "The Godfather",
+      image: " "
+    },
+    {
+      id: 3,
+      title: "The Dark Knight",
+      image: " "
+    },
+    { 
+      id: 4,
+      title: "Pulp Fiction",
+      image: " "
+    },
+    { 
+      id: 5,
+      title: "The Lord of the Rings: The Fellowship of the Ring",
+      image: " "
+    }
+  ];
+  //
+  
---
+++ artology/RandomItem/Question.js
@@ -0,0 +1,14 @@
+export let questions = [
+    {
+      id: 1,
+      question: "Find the linkage between all:"
+    },
+    {
+      id: 2,
+      question: "Create an app idea that includes:"
+    },
+    {
+      id: 3,
+      question: "Think of a story that is inspired by:"
+    },
+  ];
