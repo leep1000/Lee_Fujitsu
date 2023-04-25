bb1c369e8e1e87c332a87ee18907ee75170368f6
Katim Touray <katim70@hotmail.co.uk>
2023-04-25 15:18:30 +0100

    half arrays

---
+++ artology/RandomItem/Adjective.js
@@ -0,0 +1,28 @@
+
+export let adjectives = [
+{
+id: 1,
+word: "Surreal",
+image " "
+},
+{
+id: 2,
+word: "Melancholy",
+image " "
+},
+{
+id: 3,
+word: "Joyous",
+image " "
+},
+{
+id: 4,
+word: "Euphoric",
+image " "
+},
+{
+id: 5,
+word: "Scared",
+image " "
+}
+];
---
+++ artology/RandomItem/Place.js
@@ -0,0 +1,29 @@
+
+export let places = [
+  {
+    id: 1,
+    name: "Your room on a rainy day with the lights dimmed and your favorite music playing",
+    description: "A cozy place to relax and unwind.",
+  },
+  {
+    id: 2,
+    name: "the streets of Rio de Janeiro, Brazil during carinval",
+    description: "A vibrant city with stunning beaches, mountains, and a rich cultural history.",
+    image ""
+  },
+  {
+    id: 3,
+    name: " A well lit cafe in Paris, France",
+    description: "A romantic city with a rich history and beautiful architecture.",
+  },
+  {
+    id: 4,
+    name: "At the center of the universe, drifting in the middle of the Milky Way",
+    description: "A place where you can see the stars and planets in all their glory.",
+  },
+  {
+    id: 5,
+    name: "With the love of your life, in a beautiful meadow surrounded by flowers",
+    description: "A place where you can be yourself and be loved for who you are.",
+  }
+];
