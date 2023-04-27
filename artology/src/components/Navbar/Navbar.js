35ed93b02cb5675c20c4d488ee0571efdaee4f32
leep1000 <leepalmerformal@gmail.com>
2023-04-27 16:24:58 +0100

    navbar and scroll arrow kinda working

--- artology/src/components/Navbar/Navbar.js
+++ artology/src/components/Navbar/Navbar.js
@@ -2,13 +2,41 @@ import React from "react";
 import "./Navbar.css";
 import logo from "../../RandomItem/Image/logo.jpg";
 
-
 function Navbar() {
   function handleClick(event, id) {
-    event.preventDefault();
-    console.log("The link was clicked.");
-    console.log(id);
-    document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+    event.preventDefault(); // prevent the default anchor link behavior
+
+    // select the target element and the navbar
+    const element = document.getElementById(id);
+    const navbar = document.querySelector(".nav-bar");
+
+    // get the styles applied to the navbar
+    const navbarStyles = window.getComputedStyle(navbar);
+
+    // calculate the total height of the navbar including padding
+    const navbarHeight =
+      navbar.offsetHeight +
+      parseInt(navbarStyles.paddingTop) + // convert paddingTop to integer
+      parseInt(navbarStyles.paddingBottom); // convert paddingBottom to integer
+
+    // get the position of the body element from the top of the viewport
+    const bodyRect = document.body.getBoundingClientRect().top;
+
+    // get the position of the target element from the top of the viewport
+    const elementRect = element.getBoundingClientRect().top;
+
+    // calculate the absolute position of the target element with respect to the top of the body
+    const elementPosition = elementRect - bodyRect;
+
+    // adjust the scroll position to account for the navbar height
+    // The + 135 is personal preference to how close the top of section is below the navBar, adjust as desired
+    const offsetPosition = elementPosition - navbarHeight + 135;
+
+    // scroll the page to the calculated position
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: "smooth",
+    });
   }
 
   return (
