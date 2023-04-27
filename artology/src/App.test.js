5fceb317b119ffe767aa3fb471280aa3903ea259
HildaSig <hilda-sig@hotmail.com>
2023-04-27 15:00:23 +0100

    Wrote test for artist-studio button

--- artology/src/App.test.js
+++ artology/src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "./App";
+import RandomItem from "./components/RandomItem";
 
 
 test("Does the scroll arrow appear on page and scroll to next section", () => {
@@ -24,59 +25,81 @@ test("Does the scroll arrow appear on page and scroll to next section", () => {
 
 // check if the page loads up with everything in the right place, added an ID to the div class name for it to work 👍
 
-  test("checks if the page loads up", () => {
+test("checks if the page loads up", () => {
     const { getByTestId } = render(<App />);
     // 
     const appComponent = getByTestId("app-component");
     expect(appComponent).toBeInTheDocument();
-  });
+});
   
-  test("Does the get random item array1 button work", () => { 
-    render(<App />);
-  
-    // Is the button on the page 
-    const button = screen.getByRole("button", { name: "Get Random Item 1" });
-    expect(button).toBeInTheDocument();
-  
-    // When clicked, does the button change the item 
-    // User clicks the button
+test("Does the get random item button change image and text", () => { 
+  const getRandomItem = jest.fn();
+    render(<RandomItem item={{name:"Melancholy", image:"Image showing"}} getRandomItem={getRandomItem}/>);
+   // Assert that the heading matches the text given
+   const heading = screen.getByRole('heading');
+   expect(heading).toHaveTextContent("Melancholy");
+   // Assert that the image contains the scr passed to it 
+  const image = screen.getByRole('img');
+  expect(image).toHaveAttribute("src", "Image showing");
+   // Assert that when button is clicked, the getRandomItem function is called
+    const button = screen.getByRole('button');
+    expect(getRandomItem).not.toHaveBeenCalled();
     userEvent.click(button);
-    // We expect the item to be different
-    const item = screen.getByTestId("item1");
-    expect(item).not.toHaveTextContent("item1");
+    expect(getRandomItem).toHaveBeenCalled();
+
+
+    // Is the button on the page 
+    // const button = screen.getByRole('button', { name: "Change me!"});
+    // expect(button).toBeInTheDocument();
+  
+    //  // Get the initial image and text 
+    //  const image = screen.getByRole('img');
+    //  const text = screen.getByRole('heading');
+
+    // // When clicked, does the button change the item 
+    // // User clicks the button
+    // userEvent.click(button);
+
+    // // Get the updated image and text
+    // const updatedImage = screen.getByRole('img');
+    // const updatedText = screen.getByRole('heading');
+
+    // // We expect the item to be different
+    // expect(updatedImage).not.toBe(image);
+    // expect(updatedText).not.toBe(text);
   }); 
   
-  test("Does the get random item array2 button work", () => {
-    render(<App />);
+// test("Does the get random item array2 button work", () => {
+//     render(<App />);
   
-    // Is the button on the page 
-    const button = screen.getByRole("button", { name: "Get Random Item 2" });
-    expect(button).toBeInTheDocument();
+//     // Is the button on the page 
+//     const button = screen.getByRole("button", { name: "Get Random Item 2" });
+//     expect(button).toBeInTheDocument();
   
-    // When clicked, does the button change the item 
-    // User clicks the button
-    userEvent.click(button);
-    // We expect the item to be different
-    const item = screen.getByTestId("item2");
-    expect(item).not.toHaveTextContent("item2");
-  }); 
+//     // When clicked, does the button change the item 
+//     // User clicks the button
+//     userEvent.click(button);
+//     // We expect the item to be different
+//     const item = screen.getByTestId("item2");
+//     expect(item).not.toHaveTextContent("item2");
+//   }); 
   
-  test("Does the get random item array3 button work", () => {
-    render(<App />);
+// test("Does the get random item array3 button work", () => {
+//     render(<App />);
   
-    // Is the button on the page 
-    const button = screen.getByRole("button", { name: "Get Random Item 3" });
-    expect(button).toBeInTheDocument();
+//     // Is the button on the page 
+//     const button = screen.getByRole("button", { name: "Get Random Item 3" });
+//     expect(button).toBeInTheDocument();
   
-    // When clicked, does the button change the item 
-    // User clicks the button
-    userEvent.click(button);
-    // We expect the item to be different
-    const item = screen.getByTestId("item3");
-    expect(item).not.toHaveTextContent("item3");
-  });
+//     // When clicked, does the button change the item 
+//     // User clicks the button
+//     userEvent.click(button);
+//     // We expect the item to be different
+//     const item = screen.getByTestId("item3");
+//     expect(item).not.toHaveTextContent("item3");
+//   });
   
-  test("Does the get random question button work", () => {
+test("Does the get random question button work", () => {
     render(<App />);
   
     // Is the button on the page 
